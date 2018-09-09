1b0435c3afb7ece1a171f74393a852a8f8bda477
Renata Jachtoma Mendak <prana861@gmail.com>
2018-09-09 17:49:09 +0200

    first commit

---
+++ src/app/user.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>hello</p>
+    <p>I'userComponent</p>
+  `
+})
+export class UserComponent {
+
+}
